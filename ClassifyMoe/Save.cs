55977f68e556618d4a62d5e677579309de2d6f6b
Hugo Klaassens <hugo.klaassens@gmail.com>
2017-02-06 14:39:35 +0100

    Changed output to csv

--- ClassifyMoe/Save.cs
+++ ClassifyMoe/Save.cs
@@ -9,54 +9,51 @@ namespace ClassifyMoe
     public Save (string path, DataGridView input)
     {
       //string[] fileDataToWrite = new string[10];
-      string[] fileDataToWrite = new string[input.RowCount];
+      string[] fileDataToWrite = new string[input.RowCount + 1];
+      fileDataToWrite[0] = "CHARACTERS,SERIES,TAGS,FILENAME,DATEMODIFIED";
 
-      for (int i = 0; i < input.RowCount; i++)
+      for (int i = 1; i < input.RowCount; i++)
       {
         if (input.Rows[i].Cells[0].Value != null) //characters
         {
           fileDataToWrite[i] = input.Rows[i].Cells[0].Value.ToString ();
         }
 
+        fileDataToWrite[i] += ",";
+
         if (input.Rows[i].Cells[1].Value != null && input.Rows[i].Cells[1].Value.ToString ().Length != 0) //series
         {
-          if (fileDataToWrite[i] != null && fileDataToWrite[i].Length != 0)
-          {
-            fileDataToWrite[i] += " ";
-          }
-
-          fileDataToWrite[i] += "(" + input.Rows[i].Cells[1].Value.ToString () + ")";
+          fileDataToWrite[i] += input.Rows[i].Cells[1].Value.ToString ();
         }
 
-        if (input.Rows[i].Cells[2].Value != null && input.Rows[i].Cells[2].Value.ToString().Length != 0) //tags
-        {
-          if (fileDataToWrite[i] != null && fileDataToWrite[i].Length != 0)
-          {
-            fileDataToWrite[i] += " ";
-          }
+        fileDataToWrite[i] += ",";
 
+        if (input.Rows[i].Cells[2].Value != null && input.Rows[i].Cells[2].Value.ToString ().Length != 0) //tags
+        {
           fileDataToWrite[i] += input.Rows[i].Cells[2].Value.ToString ();
         }
 
+        fileDataToWrite[i] += ",";
+
         if (input.Rows[i].Cells[3].Value != null && input.Rows[i].Cells[3].Value.ToString ().Length != 0) //filename
         {
-          if (fileDataToWrite[i] != null && fileDataToWrite[i].Length != 0)
-          {
-            fileDataToWrite[i] += " - " + input.Rows[i].Cells[3].Value.ToString ();
-          }
-          else
-          {
-            fileDataToWrite[i] = input.Rows[i].Cells[3].Value.ToString ();
-          }
+          fileDataToWrite[i] = input.Rows[i].Cells[3].Value.ToString ();
         }
 
-        Console.WriteLine (fileDataToWrite[i]);
+        fileDataToWrite[i] += ",";
+
+        if (input.Rows[i].Cells[3].Value != null && input.Rows[i].Cells[3].Value.ToString ().Length != 0) //date modified
+        {
+          fileDataToWrite[i] = input.Rows[i].Cells[4].Value.ToString ();
+        }
+
+          Console.WriteLine (fileDataToWrite[i]);
       }
 
       Console.WriteLine ("Data to write: \n" + fileDataToWrite[0]);
       Console.WriteLine ("Input: \n" + input.Rows[5].Cells[0].Value.ToString ());
 
-      File.WriteAllLines (path + "\\ClassifyMoe.poi", fileDataToWrite);
+      File.WriteAllLines (path + "\\ClassifyMoe.csv", fileDataToWrite);
     }
 
   }
